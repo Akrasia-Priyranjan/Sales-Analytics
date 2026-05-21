f4cdb9f5b472bcce7fc14f2f823b62ad7aad170e
Akrasia-Priyranjan <xtentionholly@gmail.com>
2026-05-21 05:38:01 +0530

    Secure Aiven DB config

--- backend/db_config.py
+++ backend/db_config.py
@@ -1,5 +1,8 @@
+import os
+
 def init_db(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'pri'  # apna password
-    app.config['MYSQL_DB'] = 'sales_db'
+    app.config['MYSQL_HOST'] = 'mysql-....aivencloud.com'
+    app.config['MYSQL_USER'] = 'avnadmin'
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    app.config['MYSQL_DB'] = 'defaultdb'
+    app.config['MYSQL_PORT'] = 14702
